480078c53c51ceaafda1d61b20074e013a7987e5
Catalin Perticas <perticas_catalin@yahoo.com>
2019-01-25 23:17:37 +0200

    todo

--- generator.py
+++ generator.py
@@ -150,6 +150,9 @@ with tf.Session() as sess:
 					logits, y_exp, x = sess.run([logits_eye, y_eye, X_val])
 					# this requires special print
 					#print logits[0]
+
+					# todo: migrate printing to an analysis module 
+					# which stores interesting predictions for your study
 					out = list()
 					for j in range(10):
 						f_max = 0
