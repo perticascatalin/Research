27f526f8d1a4913d017e431e301d13ce69a44f8d
Catalin Perticas <perticas_catalin@yahoo.com>
2019-01-26 19:20:01 +0200

    cleanup

--- analysis.py
+++ analysis.py
@@ -5,22 +5,19 @@ N_CLASSES = 16
 
 def debugger(correct_pred, logits, y_exp, x):
 	print correct_pred[0]
-	# print len(logits)
-	# print logits[0]
-	# print len(logits[0])
-	out = list()
-	for j in range(N_CLASSES):
-		f_max = np.argmin(logits[0][j])
-		f_max_2 = np.argmin(logits[0][j])
-		for k in range(N_CLASSES):
-			if logits[j][0][k] > logits[j][0][f_max]:
-				f_max_2 = f_max
-				f_max = k
-			elif logits[j][0][k] > logits[j][0][f_max_2]:
-				f_max_2 = k
-		out.append((f_max, f_max_2))
-	#print out
-
+	# to check first and second choice
+	# out = list()
+	# for j in range(N_CLASSES):
+	# 	f_max = np.argmin(logits[0][j])
+	# 	f_max_2 = np.argmin(logits[0][j])
+	# 	for k in range(N_CLASSES):
+	# 		if logits[j][0][k] > logits[j][0][f_max]:
+	# 			f_max_2 = f_max
+	# 			f_max = k
+	# 		elif logits[j][0][k] > logits[j][0][f_max_2]:
+	# 			f_max_2 = k
+	# 	out.append((f_max, f_max_2))
+	# print out
 
 def pretty_printing(correct_pred, logits, y_exp, x):
 	out = list()
