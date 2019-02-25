b8a3dd4d3ab2a0baa9b975dd7dea20d6db582748
Catalin Perticas <perticas_catalin@yahoo.com>
2019-02-25 18:02:20 +0200

    comments setup

--- Permutation/setup.py
+++ Permutation/setup.py
@@ -1,8 +1,13 @@
+# For instance N = 10 (wrt position)
+# Inputs   1 2 3 4 5 6 7 8 9 10
+# Generate 2 4 3 1 8 9 6 7 5 10
 def num_classes():
 	return 10
 
+# Number of arrays to generate
 def num_samples():
 	return 10000
 
+# Maximum number in array
 def maxint():
 	return 50
