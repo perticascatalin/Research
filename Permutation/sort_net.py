58417190f83ef07cb020c432bc70a7b943a64607
Catalin Perticas <perticas_catalin@yahoo.com>
2019-04-22 15:07:31 +0300

    try on smaller example

--- Permutation/sort_net.py
+++ Permutation/sort_net.py
@@ -25,24 +25,54 @@ lsts_train, orders_train = tf.train.slice_input_producer([lsts_train, orders_tra
 X, Y = tf.train.batch([lsts_train, orders_train], batch_size = batch_size, capacity = batch_size * 8, num_threads = 4)
 
 #logits_train, y_train = neural_net(X, Y, N_OUT_CLASSES, N_CLASSES, dropout, reuse = False, is_training = False)
-ts = np.zeros((N_CLASSES, N_CLASSES, N_CLASSES, ), np.float32)
+ts = np.zeros((N_CLASSES, N_CLASSES, N_CLASSES), np.float32)
 for i in range(N_CLASSES):
 	for j in range(N_CLASSES):
 		if i == j:
 			continue
-
 		ts[i][j][i] = 1.0
 		ts[i][j][j] = -1.0
 
 print ts[:,0,1], ts[:,1,2], ts[:,1,3]
 v = tf.Variable(ts, trainable = False)
-v = tf.expand_dims(v, 0)
-X = tf.transpose(X)
-X = tf.expand_dims(X, 0)
-X = tf.expand_dims(X, 3)
 print v.shape
 print X.shape
-res = tf.matmul(v, X)
+
+#res = tf.matmul(v, X)
+
+# # Initialize the variables (i.e. assign their default value)
+# init = tf.global_variables_initializer()
+# saver = tf.train.Saver()
+
+# with tf.Session() as sess:
+# 	# Run the initializer
+# 	sess.run(init)
+# 	# Start the data queue
+# 	coord = tf.train.Coordinator()
+# 	threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+
+# 	sol = sess.run([res])
+# 	pdb.set_trace()
+
+n = 3
+m = 2
+x = np.zeros((m,n), np.float32)
+x[0][0] = 21
+x[0][1] = 5
+x[0][2] = 16
+# n*n = 9
+y = np.zeros((n,n*n), np.float32)
+for i in range(n):
+	for j in range(n):
+		if i == j:
+			continue
+		y[i][i*n + j] = 1.0
+		y[j][i*n + j] = -1.0
+
+X = tf.Variable(x, trainable = False)
+Y = tf.Variable(y, trainable = False)
+
+R = tf.matmul(X, Y)
 
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
@@ -55,6 +85,13 @@ with tf.Session() as sess:
 	coord = tf.train.Coordinator()
 	threads = tf.train.start_queue_runners(sess = sess, coord = coord)
 
-	sol = sess.run([res])
-	pdb.set_trace()
+	sol = sess.run([R])
+	print len(sol)
+	print len(sol[0])
+	print len(sol[0][0])
+	print sol[0][0]
+	print sol[0][1]
+	sol = np.reshape(sol, (m,n,n))
+	print sol
+	#pdb.set_trace()
 
