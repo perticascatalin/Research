86dbdd4c709c2e7d5d647ee4c2ab4a3a75d6697a
Catalin Perticas <perticas_catalin@yahoo.com>
2022-04-28 21:14:07 +0300

    add validation loss

--- PermutationRN/analysis.py
+++ PermutationRN/analysis.py
@@ -230,13 +230,14 @@ def print_acc_design():
 	plt.savefig('./results/' + 'ad.png')
 	plt.clf()
 
-def print_ltv(A, B, C, x, filename):
+def print_ltv(A, B, C, D, x, filename):
 	plt.title('Loss and Accuracy', fontsize = 18)
 	plt.xlabel('# Steps', fontsize = 16)
 	plt.ylabel('% Value', fontsize = 16)
-	plt.plot(x, A, 'blue', linewidth = 1.0, label = 'Loss')
-	plt.plot(x, B, 'orange', linewidth = 1.0, label = 'Training Accuracy')
-	plt.plot(x, C, 'red', linewidth = 1.0, label = 'Validation Accuracy')
+	plt.plot(x, A, 'blue', linewidth = 1.0, label = 'Training Loss')
+	plt.plot(x, B, 'cyan', linewidth = 1.0, label = 'Validation Loss')
+	plt.plot(x, C, 'orange', linewidth = 1.0, label = 'Training Accuracy')
+	plt.plot(x, D, 'red', linewidth = 1.0, label = 'Validation Accuracy')
 	# Remove legend altogether
 	#plt.legend(loc = 'upper left')
 	plt.savefig('./results/loss_and_acc/' + filename)
--- PermutationRN/neural_net.py
+++ PermutationRN/neural_net.py
@@ -24,8 +24,7 @@ data_type = stp.data_type
 #model_name = "baseline"
 #model_name = "design"
 #model_name = "lis"
-model_name = "test" # 10
-model_name = "test_20"
+model_name = "test"
 
 # General dropout, initially applied to all layers
 dropout = 0.0
@@ -107,17 +106,25 @@ X_val, Y_val = tf.train.batch([lsts_val, orders_val], batch_size = batch_size, c
 # Define the logits for all datasets
 logits_train, y_train = neural_net(X, Y, N_OUT_CLASSES, N_CLASSES, dropout, reuse = False, is_training = True)
 logits_test, y_test = neural_net(X, Y, N_OUT_CLASSES, N_CLASSES, dropout, reuse = True, is_training = False)
+# Note: valt is val with is_training set to True, important for getting the actual validation loss
+# Similar to train vs test
+logits_valt, y_valt = neural_net(X_val, Y_val, N_OUT_CLASSES, N_CLASSES, dropout, reuse = True, is_training = True)
 logits_val, y_val = neural_net(X_val, Y_val, N_OUT_CLASSES, N_CLASSES, dropout, reuse = True, is_training = False)
 
 # Define the loss operation
-loss_op = tf.constant(0.0, dtype = tf.float32)
+train_loss_op = tf.constant(0.0, dtype = tf.float32)
 for i in range(N_OUT_CLASSES):
-	loss_op = loss_op + tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(\
+	train_loss_op = train_loss_op + tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(\
 	logits = logits_train[i], labels = Y[:,i]))
+
+val_loss_op = tf.constant(0.0, dtype = tf.float32)
+for i in range(N_OUT_CLASSES):
+	val_loss_op = val_loss_op + tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(\
+	logits = logits_valt[i], labels = Y_val[:,i]))
 
 # Define loss and optimizer (with train logits, for dropout to take effect)
 optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
-train_op = optimizer.minimize(loss_op)
+train_op = optimizer.minimize(train_loss_op)
 
 # Define loss for prediction on training dataset
 correct_pred_train = tf.constant(0.0, dtype = tf.float32)
@@ -125,7 +132,7 @@ for i in range(N_OUT_CLASSES):
 	correct_pred_train = correct_pred_train + tf.cast(tf.equal(tf.argmax(logits_test[i], 1), tf.cast(Y[:,i], tf.int64)), tf.float32)
 accuracy_train = tf.reduce_mean(correct_pred_train)
 
-# Define loss for prediction ong validation dataset
+# Define loss for prediction on validation dataset
 correct_pred_val = tf.constant(0.0, dtype = tf.float32)
 for i in range(N_OUT_CLASSES):
 	correct_pred_val = correct_pred_val + tf.cast(tf.equal(tf.argmax(logits_val[i], 1), tf.cast(Y_val[:,i], tf.int64)), tf.float32)
@@ -142,7 +149,8 @@ with tf.Session() as sess:
 	coord = tf.train.Coordinator()
 	threads = tf.train.start_queue_runners(sess = sess, coord = coord)
 
-	losses = []
+	train_losses = []
+	val_losses = []
 	train_accs = []
 	val_accs = []
 	steps = []
@@ -153,14 +161,15 @@ with tf.Session() as sess:
 			# Run optimization
 			sess.run([train_op])
 			# Calculate average batch loss and accuracy
-			total_loss = 0.0
+			training_loss = 0.0
+			validation_loss = 0.0
 			training_accuracy = 0.0
 			validation_accuracy = 0.0
 
 			# Each step walks through 100 x batch_size number of samples
 			# Covers 12800/60000 = ~20% of dataset in an interation
 			for i in range(100):
-				loss, acc_train, acc_val = sess.run([loss_op, accuracy_train, accuracy_val])
+				loss_train, loss_val, acc_train, acc_val = sess.run([train_loss_op, val_loss_op, accuracy_train, accuracy_val])
 				if i == 0:
 					correct_pred, logits, y_exp, x = sess.run([correct_pred_val, logits_val, Y_val, X_val])
 					co.debugger(correct_pred, logits, y_exp, x)
@@ -168,20 +177,24 @@ with tf.Session() as sess:
 					# also count strictly correctly sorted (uncomment next line)
 					#co.print_pretty(correct_pred, logits, y_exp, x, step, True)
 				
-				total_loss += loss
+				training_loss += loss_train
+				validation_loss += loss_val
 				training_accuracy += acc_train
 				validation_accuracy += acc_val
 
-			total_loss /= 100.0
+			training_loss /= 100.0
+			validation_loss /= 100.0
 			training_accuracy /= 100.0    
 			validation_accuracy /= 100.0
 
-			print("Step " + str(step) + ", Loss= " + \
-				"{:.4f}".format(total_loss) + ", Training Accuracy= " + \
-				"{:.3f}".format(training_accuracy) + ", Validation Accuracy= " + \
-				"{:.3f}".format(validation_accuracy))
+			print("Step " + str(step) + \
+				", Training Loss= "       + "{:.4f}".format(training_loss) + \
+				", Validation Loss= "     + "{:.4f}".format(validation_loss) + \
+				", Training Accuracy= "   + "{:.3f}".format(training_accuracy) + \
+				", Validation Accuracy= " + "{:.3f}".format(validation_accuracy))
 
-			losses.append(total_loss)
+			train_losses.append(training_loss)
+			val_losses.append(validation_loss)
 			train_accs.append(100.0*training_accuracy/N_CLASSES)
 			val_accs.append(100.0*validation_accuracy/N_CLASSES)
 			steps.append(step/1000)
@@ -191,12 +204,13 @@ with tf.Session() as sess:
 
 	print("Optimization Finished!")
 	# Dump additional data for later investigation
-	pickle.dump(losses, open('./data/stats/' + model_name + '_ml_losses.p', 'wb'))
+	pickle.dump(train_losses, open('./data/stats/' + model_name + '_ml_t_losses.p', 'wb'))
+	pickle.dump(val_losses, open('./data/stats/' + model_name + '_ml_v_losses.p', 'wb'))
 	pickle.dump(train_accs, open('./data/stats/' + model_name + '_ml_t_accs.p', 'wb'))
 	pickle.dump(val_accs, open('./data/stats/' + model_name + '_ml_v_accs.p', 'wb'))
 	pickle.dump(steps, open('./data/stats/' + model_name + '_ml_steps.p', 'wb'))
 	# Or just plot it
-	co.print_ltv(losses, train_accs, val_accs, steps, model_name + '_sample.png')
+	co.print_ltv(train_losses, val_losses, train_accs, val_accs, steps, model_name + '_sample.png')
 	# Save your model
 	saver.save(sess, './checkpts/')
 	# Stop threads
