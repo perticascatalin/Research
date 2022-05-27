85686904c5fc4d7660cc0c37cc2be3dac2bd8642
Catalin Perticas <perticas_catalin@yahoo.com>
2022-05-27 22:13:14 +0300

    simplify

--- PermutationRN/nets_inspect.py
+++ PermutationRN/nets_inspect.py
@@ -11,9 +11,9 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Setup experiment size and parameters
 N_CLASSES = conf.num_inputs
 N_OUT_CLASSES = conf.num_outputs
-batch_size = 8
+batch_size = 5
 data_type = conf.data_type
-model_name = "exp_8"
+model_name = "exp_3"
 
 print "GENERATE TRAINING DATA"
 lsts_train, orders_train = gen.data_by_type(data_type, is_training = True)
@@ -21,16 +21,9 @@ lsts_train = tf.convert_to_tensor(lsts_train, dtype = tf.float32)
 orders_train = tf.convert_to_tensor(orders_train, dtype = tf.int32)
 lsts_train, orders_train = tf.train.slice_input_producer([lsts_train, orders_train], shuffle = True)
 
-print "GENERATE VALIDATION DATA"
-lsts_val, orders_val = gen.data_by_type(data_type, is_training = False)
-lsts_val = tf.convert_to_tensor(lsts_val, dtype = tf.float32)
-orders_val = tf.convert_to_tensor(orders_val, dtype = tf.int32)
-lsts_val, orders_val = tf.train.slice_input_producer([lsts_val, orders_val], shuffle = True)
-
 X, Y = tf.train.batch([lsts_train, orders_train], batch_size = batch_size, capacity = batch_size * 8, num_threads = 4)
-X_val, Y_val = tf.train.batch([lsts_val, orders_val], batch_size = batch_size, capacity = batch_size * 8, num_threads = 4)
 
-units_a, units_b, units_c, units_d, units_e = mod.net_inspect(X,     N_OUT_CLASSES, N_CLASSES, batch_size, reuse = False, is_training = True)
+units_a, units_b, units_c, units_d, units_e = mod.net_inspect(X, N_OUT_CLASSES, N_CLASSES, batch_size, reuse = False, is_training = True)
 
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
